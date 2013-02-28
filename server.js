481cc8daa71f0fae9fd446f567e7a7f9ccecab51
Nick Crohn <ncrohn@me.com>
2013-02-28 13:29:14 -0700

    games should all be getting watched and get updated

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
-var st = new require('./index').ScoreTracker();
+var ScoreTracker = require('./index').ScoreTracker;
+
+var st = new ScoreTracker();
+
+st.on('gameChange', function(game) {
+  console.log('Game Change:');
+  console.log(game);
+});
 
 st.watch();
+
+console.log('Watching ESPN Game Feed');
